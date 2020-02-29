bdef20f18abb21d052c8ba4bdd8c923158409c87
蒋宇涵 <jiangyuhan0316@outlook.com>
2020-02-29 11:57:27 +0800

    Add files via upload

---
+++ democode/demo.c
@@ -0,0 +1,51 @@
+
+/***************************************************************
+ VisualStudio 2010 用户注意：
+    将Character Set设置为 Use Multibyte Character Set
+	否则中文显示不正常
+***************************************************************/
+
+#define Demo_ID 12
+
+#if Demo_ID == 0
+#include "CAD.c"
+#include "linkedlist.c"
+
+#elif Demo_ID == 1
+#include "house.c"
+
+#elif Demo_ID == 2
+#include "kochsnow.c"
+#include "linkedlist.c"
+
+#elif Demo_ID == 3
+#include "igp.c"
+
+#elif Demo_ID == 4
+#include "igp_char.c"
+
+#elif Demo_ID == 5
+#include "igp_keyboard.c"
+
+#elif Demo_ID == 6
+include "igp_mouse.c"
+
+#elif Demo_ID == 7
+#include "igp_timer.c"
+
+#elif Demo_ID == 8
+#include "demoGuiButton.c"
+
+#elif Demo_ID == 9
+#include "demoGuiMenu.c"
+
+#elif Demo_ID == 10
+#include "demoGuiTextInput.c"
+
+#elif Demo_ID == 11
+#include "demoGuiRotate.c"
+
+#elif Demo_ID == 12
+#include "demoGuiALL.c"
+
+#endif 
